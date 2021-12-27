39bf062801c8298f3082325f49613bada0c057e3
Starnino <francesco.starna@mail.com>
2021-12-27 17:21:52 +0100

    changed plots

--- plots.m
+++ plots.m
@@ -12,3 +12,4 @@ plot(Attitude.time,Attitude.signals.values(:,1),'r');
 plot(Attitude.time,Attitude.signals.values(:,2),'g');
 plot(Attitude.time,Attitude.signals.values(:,3),'b');
 grid on
+hold off
