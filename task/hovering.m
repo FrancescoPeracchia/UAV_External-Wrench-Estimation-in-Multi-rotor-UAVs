e3f72659d3fabd90b7fe350cc2ec709c4de2e1ed
FrancescoVIncelli <50082120+FrancescoVIncelli@users.noreply.github.com>
2022-04-02 08:31:38 +0100

    Add files via upload

--- task/hovering.m
+++ task/hovering.m
@@ -1,4 +1,10 @@
 function trajectory = hovering(h)
+    % ELICOIDAL TRAJECOTRY
+    % ARGS:
+    %   - height of the vertical path
+    % RETRUN:
+    %   - quadcopter state at time @t: only the position changes, while
+    %     velocity and accelerarion remain constants
     position = [0, 0, h];
     trajectory = @(t)[position; zeros(1,3); zeros(1,3); zeros(1,3)];
 end
